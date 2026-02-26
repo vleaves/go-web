0ddb2923b5c3476d7fb544c368c12cc704c914dc
kleaves <13708799+arrow-feather@user.noreply.gitee.com>
2026-02-26 21:35:20 +0800

    请求处理

--- 03_handle_request/process.go
+++ 03_handle_request/process.go
@@ -7,9 +7,10 @@ import (
 )
 
 func Process(w http.ResponseWriter, r *http.Request) {
-	r.ParseMultipartForm(1024)
-	fileHeader := r.MultipartForm.File["uploaded"][0]
-	file, err := fileHeader.Open()
+	// r.ParseMultipartForm(1024)
+	// fileHeader := r.MultipartForm.File["uploaded"][0]
+	// file, err := fileHeader.Open()
+	file, _, err := r.FormFile("uploaded")
 	if err == nil {
 		data, err := io.ReadAll(file)
 		if err == nil {
